36f52865ce52762a8872b62f86a29b17d9037074
Juan José <juanjose.montesdeocaarbos@gmail.com>
2018-03-07 14:05:57 -0300

    Add second test: logic Machine change to resolve all tests

--- VendingMachine/VendingMachine.Domain/Machine.cs
+++ VendingMachine/VendingMachine.Domain/Machine.cs
@@ -4,13 +4,41 @@ namespace VendingMachine.Domain
 {
     public class Machine
     {
+        private int[] _billetes;
+
+        public Machine()
+        {
+            _billetes = new int[5] { 100, 50, 20, 10, 5 };
+        }
+
         public IDictionary<int, int> GetChange(decimal price, decimal pay)
         {
-            return new Dictionary<int, int>
+            var change = new Dictionary<int, int>();
+
+            pay -= price;
+            while (pay > 0)
             {
-                {50, 1},
-                {20, 2},
-            };
+                foreach (var billete in _billetes)
+                {
+                    if (pay - billete >= 0)
+                    {
+                        AgregarAlVuelto(billete, ref change);
+                        pay -= billete;
+                        break;
+                    }
+                }
+            }
+
+            return change;
+        }
+
+        private void AgregarAlVuelto(int billete, ref Dictionary<int, int> change)
+        {
+            if (change.ContainsKey(billete))
+                change[billete]++;
+            else
+                change.Add(billete, 1);
         }
     }
+
 }
--- VendingMachine/VendingMachine.Tests/MachineTests.cs
+++ VendingMachine/VendingMachine.Tests/MachineTests.cs
@@ -18,5 +18,19 @@ namespace VendingMachine.Tests
             Assert.AreEqual(2, change[20]);
             Assert.AreEqual(1, change[50]);
         }
+
+        [Test]
+        public void PayWith100_Cost20_Returns50_20_10()
+        {
+            var machine = new Machine();
+
+            var change = machine.GetChange(20, 100);
+
+            Assert.AreEqual(3, change.Count());
+            Assert.AreEqual(1, change[10]);
+            Assert.AreEqual(1, change[20]);
+            Assert.AreEqual(1, change[50]);
+        }
+
     }
 }
