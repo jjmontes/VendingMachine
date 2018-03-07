48d7f2767c028caf4abc6a205a37a11424b02965
Juan José <juanjose.montesdeocaarbos@gmail.com>
2018-03-07 13:55:14 -0300

    Add first test: test green quick and dirty

---
+++ VendingMachine/VendingMachine.Domain/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace VendingMachine.Domain
+{
+    public class Machine
+    {
+        public IDictionary<int, int> GetChange(decimal price, decimal pay)
+        {
+            return new Dictionary<int, int>
+            {
+                {50, 1},
+                {20, 2},
+            };
+        }
+    }
+}
--- VendingMachine/VendingMachine.Tests/MachineTests.cs
+++ VendingMachine/VendingMachine.Tests/MachineTests.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NUnit.Framework;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using VendingMachine.Domain;
 
 namespace VendingMachine.Tests
 {
+    [TestFixture]
     public class MachineTests
     {
+        [Test]
+        public void PayWith100_Cost10_Returns50_20_20()
+        {
+            var machine = new Machine();
+
+            var change = machine.GetChange(10, 100);
+
+            Assert.AreEqual(2, change.Count());
+            Assert.AreEqual(2, change[20]);
+            Assert.AreEqual(1, change[50]);
+        }
     }
 }
